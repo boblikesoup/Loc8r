61c8976d3928d3d5147349578edb580fe4087fb9
boblikesoup <johnuke@gmail.com>
2015-02-10 17:37:00 -0800

    Styled landing page and location info page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,12 +3,12 @@ var router = module.exports = express.Router();
 
 // Index - Home List
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('locations-list', { title: 'Home' });
 });
 
 // Location Info
-router.get('/location', function(req, res, next) {
-  res.render('index', { title: 'Location Info' });
+router.get('/location-info', function(req, res, next) {
+  res.render('location-info', { title: 'Location Info' });
 });
 
 //Add Review
